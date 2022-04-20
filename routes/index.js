ccf374231821d7825e6706c803dc77d28860f930
Chris Pietrangeli <chris.pietrangeli11@gmail.com>
2022-04-19 21:59:51 -0700

    user model

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
+
+const apiRoutes = require('./api');
+
 const htmlRoutes = require('./html/html-routes');
+
+router.use('/api', apiRoutes);
 
 router.use('/', htmlRoutes);
 
